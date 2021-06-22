1bdcbd23f35dd02027effe50d2755cee45c43fed
deargosep <70290804+deargosep@users.noreply.github.com>
2021-06-22 21:08:23 +0600

    making edit button

--- src/App.js
+++ src/App.js
@@ -1,77 +1,9 @@
-import React, { useState } from 'react';
-import { Input, Button, List, Card } from 'antd';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+  import Todo from './Pages/Todo';
 
-export default function App() {
-  const [todos, setTodo] = useState([
-    {
-      name: 'Народить котиков',
-      description:
-        'Народить много котиков всех пород, чтобы создать армию и устроить геноцид!'
-    }
-  ]);
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  function createTodo(e) {
-    e.preventDefault();
-    setTodo([...todos, { name: name, description: description }]);
-    setName('');
-    setDescription('');
-  }
-  function handleName(e) {
-    setName(e.target.value);
-  }
-  function handleDescription(e) {
-    setDescription(e.target.value);
-  }
-  function del(index) {
-    var array = [...todos]; // make a separate copy of the array
-    if (index !== -1) {
-      array.splice(index, 1);
-      setTodo(array);
-    }
-  }
-  function clear() {
-    setTodo([])
-  }
-  return (
-    <div>
-      <br/>
-      <h1 style={{textAlign: 'center'}}>Todo app</h1>
-      <Card>
-        <List
-        header="Tasks"
-          dataSource={todos}
-          renderItem={(todo, index) => (
-            <List.Item actions={[<Button onClick={() => del(index)}>Delete</Button>]}>
-            <List.Item.Meta title={todo.name} description={todo.description} />
-            </List.Item>
-          )}
-        />
-      </Card>
-      <div style={{padding: '15px'}}>
-        <form onSubmit={createTodo}>
-          <Input
-            type="text"
-            required
-            onChange={handleName}
-            name="name"
-            value={name}
-            placeholder="name"
-          />
-          <Input
-            type="text"
-            onChange={handleDescription}
-            required
-            name="description"
-            value={description}
-            placeholder="description"
-          />
-          <input style={{width: '100%'}} type="submit" />
-        </form>
-        <br/>
-        <Button style={{width: '100%'}} onClick={clear}>Clear</Button>
-        </div>
+  export default function App() {
+    return(
+    <div className="app">
+      <Todo/>
     </div>
-  );
-}
+    )
+  }
---
+++ src/Pages/Todo.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Input, Button, List, Card } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+export default function Todo() {
+  const initArray = {
+    name: 'Народить котиков',
+    description:
+      'Народить много котиков всех пород, чтобы создать армию и устроить геноцид!'
+  };
+
+  const [todos, setTodo] = useState([initArray]);
+  const [change, setChange] = useState({ isChanging: false, index: null });
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => setTodo([...todos, data]);
+  const changeSubmit = (data, index) => {
+    let changingObj = todos[index]
+    changingObj = data
+    let oldArray = todos;
+    oldArray[index] = changingObj
+    setTodo([...oldArray]);
+  }
+
+  const del = index => {
+    let oldArray = [todos];
+    oldArray.splice(index, 1);
+    setTodo([...oldArray]);
+  }
+  const startChange = index => {
+    let changeObj = {
+      isChanging: true,
+      index: index
+    }
+    setChange(changeObj)
+  }
+  return (
+    <div>
+      <br />
+      <h1 style={{ textAlign: 'center' }}>Todo app</h1>
+      <Card>
+        <List
+          header="Tasks"
+          dataSource={todos}
+          renderItem={(todo, index) => (
+            <List.Item actions={[<Button onClick={() => del(index)}>Delete</Button>, <Button onClick={() => startChange(index)}>Edit</Button>]}>
+              {change.isChanging ? <form onSubmit={handleSubmit(changeSubmit(index))}>
+          <Input
+            {...register("name")}
+            type="text"
+            required
+            name="name"
+            placeholder="name"
+          />
+          <Input
+            {...register("description")}
+            type="text"
+            required
+            name="description"
+            placeholder="description"
+          />
+          <input type="submit" />
+        </form> : <List.Item.Meta title={todo.name} description={todo.description} />}
+            </List.Item>
+          )}
+        />
+      </Card>
+      <div style={{ padding: '15px' }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            {...register("name")}
+            type="text"
+            required
+            name="name"
+            placeholder="name"
+          />
+          <Input
+            {...register("description")}
+            type="text"
+            required
+            name="description"
+            placeholder="description"
+          />
+          <input style={{ width: '100%' }} type="submit" />
+        </form>
+        <br />
+        <Button style={{ width: '100%' }} onClick={() => setTodo([])}>Clear</Button>
+      </div>
+    </div>
+  );
+}
